04e1b19f783f7bd4ed061677ae87cabc94416208
Rochet brice <brice.974452@gmail.com>
2023-07-27 09:58:09 +0200

    Fermeture du formulaire

--- main/modal.js
+++ main/modal.js
@@ -11,13 +11,20 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeForm = document.querySelector('.close');
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// close modal event
+closeForm.addEventListener('click', closing);
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-
+// close modal event
+function closing() {
+  modalbg.style.display = "none";
+}
